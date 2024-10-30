db277019786df9d7ea34a4309d08bef5b4788a68
Parth Patil <parth.patil@desy.de>
2024-10-30 15:20:20 +0100

    solution_1

--- stichprobe.cc
+++ stichprobe.cc
@@ -4,22 +4,17 @@
 int main(){
     std::ifstream inputfile("datensumme.txt");
     // std::ofstream outputfile("datensumme.txt");
-    double a,var_half,i;
+    double a,var_half;
     double b=0;
     double sum= 0;
     double var=0;
     while (inputfile >> a)
     {
-        for (i=0;i<90;i++){
-            i+=1;
-            b+=1;
-            sum += a;
-        }
-        double mean = sum/b;
-        std::cout << "Mean: " << mean << std::endl;
+        b+=1;
+        sum += a;
     }
-    
-    
+    double mean = sum/b;
+    std::cout << "Mean: " << mean << std::endl;
     
     inputfile.close();
     inputfile.open("datensumme.txt");
