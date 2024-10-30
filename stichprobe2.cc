b46f3bf80f403c49f669d47e409823e08abd3fbc
BenjaminLe99 <benjaminle21@hotmail.com>
2024-10-30 16:38:50 +0100

    fixed indentation in stichprobe2.cc

--- stichprobe2.cc
+++ stichprobe2.cc
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-int main(){
+int main()
+{
     std::ifstream inputfile("datensumme.txt");
     std::ifstream inputfile2("datensumme.txt");
     std::ofstream outputfile("mittelwerte.txt");
@@ -12,29 +13,32 @@ int main(){
     double mean_mean = 0;
     double var_mean = 0;
 
-for (int i = 0; i < 26; i++){
-    double mean= 0;
-    double var=0;
-    for (int i = 0; i < 9; i++){
-        inputfile >> a;
-        mean += a/9;   
+    for (int i = 0; i < 26; i++)
+    {
+        double mean= 0;
+        double var=0;
+            for (int i = 0; i < 9; i++)
+            {
+                inputfile >> a;
+                mean += a/9;   
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                inputfile2 >> c;
+                var_half = (c-mean)*(c-mean);
+                var += var_half/9;
+            }
+        std::cout << "Mean: " << mean << std::endl;
+        std::cout << "Variance: " << var << std::endl;
+        outputfile << "" << mean << std::endl;
+        outputfile2 << "" << var << std::endl;
+        mean_mean += mean/26;
+        var_mean += var/26;
     }
-    for (int i = 0; i < 9; i++){
-        inputfile2 >> c;
-        var_half = (c-mean)*(c-mean);
-        var += var_half/9;
-    }
-    std::cout << "Mean: " << mean << std::endl;
-    std::cout << "Variance: " << var << std::endl;
-    outputfile << "" << mean << std::endl;
-    outputfile2 << "" << var << std::endl;
-    mean_mean += mean/26;
-    var_mean += var/26;
-}
-std::cout << "Mean of mean: " << mean_mean << std::endl;
-std::cout << "Mean of Variance: " << var_mean << std::endl;
-inputfile.close();
-inputfile2.close();
-outputfile.close();
-outputfile2.close();
+    std::cout << "Mean of mean: " << mean_mean << std::endl;
+    std::cout << "Mean of Variance: " << var_mean << std::endl;
+    inputfile.close();
+    inputfile2.close();
+    outputfile.close();
+    outputfile2.close();
 }
